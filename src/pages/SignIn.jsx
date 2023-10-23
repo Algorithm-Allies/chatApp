25a78f9aa768d943bf0ab7e3509e3a8d62a1dc7e
mnicokar <nicokar.mobin@gmail.com>
2023-10-23 16:47:39 -0700

    added useState

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignIn = () => {
+  // Create state variables for email and password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Event handler to update the email state
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  // Event handler to update the password state
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+
+  
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <form action="">
-          
           <span className="identifier">Email</span>
-          <input type="text" />
+          <input type="text" value={email} onChange={handleEmailChange} />
           <span className="identifier">Password</span>
-          <input type="text" />
+          <input type="password" value={password} onChange={handlePasswordChange} />
         </form>
-        <p>Don’t have an account?  
-          <a href="#"
-          className="link"> Sign up</a>
+        <p>
+          Don’t have an account? <a href="#" className="link">Sign up</a>
         </p>
       </div>
     </div>
