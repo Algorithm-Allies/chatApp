6ea111bb846bd508d3cf5a6843041dea033c9146
williamvelichko <williamvelichko2003@gmail.com>
2023-10-24 14:56:31 -0700

    save

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,27 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './login.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 function Login() {
-    return (
-        <div className="login-page">
-            <div className="login-container">
-                <h1 className="heading">Ripple</h1>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="username" className="form-label">Username</label>
-                        <input type="text" id="username" name="username" placeholder="Enter your username" className="form-input" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" className="form-input" />
-                    </div>
-                    <button type="submit" className="login-button">Login</button>
-                </form>
-                <p className="login-create-account">Don't have an account?{' '}<Link to='/create-account' className="create-account-link">Create an account</Link></p>
-            </div>
-        </div>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChanges = (e) => {
+    if (e.target.id === "username") {
+      setUsername(e.target.value);
+    } else if (e.target.id === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login clicked!");
+    console.log("Username: ", username);
+    console.log("Password: ", password);
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h1 className="heading">Ripple</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+              className="form-input"
+              value={username}
+              onChange={handleInputChanges}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="form-input"
+              value={password}
+              onChange={handleInputChanges}
+            />
+          </div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+        <p className="login-create-account">
+          Don't have an account?
+          <Link to="/create-account" className="create-account-link">
+            Create an account
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
