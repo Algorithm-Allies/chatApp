6cf96023576e31d39218f46f387002b08f6b18be
mnicokar <nicokar.mobin@gmail.com>
2023-10-24 10:32:09 -0700

    Completed routing between Register and Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import SignIn from './pages/SignIn'
-import "./style.scss"
-import Register from './pages/Register'
+import { useState } from "react";
+import SignIn from "./pages/SignIn";
+import "./style.scss";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
   return (
-    <>
-      <Register/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="login" element={<SignIn />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
