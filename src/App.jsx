1b008896412c4d9d1aa96773480dcd353d0d69bf
williamvelichko <williamvelichko2003@gmail.com>
2023-10-31 13:22:07 -0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Login from './pages/Login'; 
 import Create from './pages/Create';
 
 import './index.css'
+=======
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Create from "./pages/Create";
+import "./pages/login.css";
+>>>>>>> fa3f56e (save)
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/create-account" element={<Create />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/create-account" element={<Create />} />
+    </Routes>
+  );
 };
 
 export default App;
-
-
-
