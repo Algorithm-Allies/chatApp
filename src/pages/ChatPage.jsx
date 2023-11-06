eb2b3a2c7739f27665cf95263a2c9ae16f19f1f5
mnicokar <nicokar.mobin@gmail.com>
2023-11-05 16:18:10 -0800

    scrollable messages displaying for general channel

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import SideBar from "../components/SideBar";
 import TextBox from "../components/TextBox";
+import ChannelMessages from "../components/channelMessages";
 function ChatPage() {
   return (
     <div className="flex flex-row h-full">
       <SideBar />
-      <TextBox />
+
+      <div className="flex flex-col h-screen w-screen bg-stone-800">
+        
+        <ChannelMessages />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {}
+    },
   },
   plugins: [],
 }
