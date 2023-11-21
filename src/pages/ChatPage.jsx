84577cf427b81a38496ed84b4e726a4785e5a3b1
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-20 17:35:55 -0800

    state for channel modal and props for children

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../components/SideBar";
 import TextBox from "../components/TextBox";
 import ChannelMessages from "../components/channelMessages";
+import CreateChannelModal from "../components/createChannelModal";
 //Chat page
 function ChatPage() {
+
+  const [showCreateChannelModal, setShowCreateChannelModal] = useState(false)
+
   return (
     <div className="flex flex-row h-full">
-      <SideBar />
+      <SideBar setShowCreateChannelModal={setShowCreateChannelModal} />
 
       <div className="flex flex-col h-screen w-screen bg-stone-800">
         
         <ChannelMessages />
       </div>
+      <CreateChannelModal showCreateChannelModal={showCreateChannelModal} setShowCreateChannelModal={setShowCreateChannelModal} />
     </div>
   );
 }
