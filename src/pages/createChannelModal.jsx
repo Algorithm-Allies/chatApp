154369090dcb41851d926913e11fb61a0c600fc7
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-20 17:11:03 -0800

    createChannelModal component

---
+++ src/pages/createChannelModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function createChannelModal() {
+  return (
+    <div>
+      A modal for creating a channel.
+    </div>
+  );
+}
+
+export default createChannelModal;
