b7d1b093c1ec17db9f0e85cffb3632fc1ef87b25
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-26 09:35:57 -0800

    cleaning up CreateChannelModal elements

--- src/components/CreateChannelModal.jsx
+++ src/components/CreateChannelModal.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
+import '../styles/CreateChannelModal.css'
 
-function CreateChannelModal({showCreateChannelModal, setShowCreateChannelModal}) {
+function CreateChannelModal({ channelModalRef, closeChannelModal }) {
 
   return (
-    showCreateChannelModal
-    ?
-    <div>
-      <p>Hello I'm a modal.</p>
-      <button onClick={() => setShowCreateChannelModal(false)}>Cancel</button>
-    </div>
-    :
-    null
+    <dialog ref={channelModalRef} >
+      <div id="channel-modal">
+        <label id="channel-modal-input-label" htmlFor="">Channel Name</label>
+        <input id="channel-modal-input" type="text" placeholder="Enter channel name here" />
+        <div id="channel-modal-buttons">
+          <button id="channel-modal-cancel" onClick={() => closeChannelModal()}>Cancel</button>
+          <button id="channel-modal-create">Create Channel</button>
+        </div>
+
+      </div>
+    </dialog>
   );
 }
 
