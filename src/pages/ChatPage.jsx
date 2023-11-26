476c78224fbf52b8f1fa57a2434b8c0120138aa4
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-26 09:35:30 -0800

    open and close modal props

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import SideBar from "../components/SideBar";
 import TextBox from "../components/TextBox";
 import ChannelMessages from "../components/channelMessages";
@@ -6,17 +6,27 @@ import CreateChannelModal from "../components/createChannelModal";
 //Chat page
 function ChatPage() {
 
-  const [showCreateChannelModal, setShowCreateChannelModal] = useState(false)
+  const channelModalRef = useRef(null);
+  const openChannelModal = () => {
+    if (channelModalRef.current) {
+      channelModalRef.current.showModal()
+    }
+  }
+  const closeChannelModal = () => {
+    if (channelModalRef.current) {
+      channelModalRef.current.close()
+    }
+  }
 
   return (
     <div className="flex flex-row h-full">
-      <SideBar setShowCreateChannelModal={setShowCreateChannelModal} />
+      <SideBar openChannelModal={openChannelModal} />
 
       <div className="flex flex-col h-screen w-screen bg-stone-800">
         
         <ChannelMessages />
       </div>
-      <CreateChannelModal showCreateChannelModal={showCreateChannelModal} setShowCreateChannelModal={setShowCreateChannelModal} />
+      <CreateChannelModal channelModalRef={channelModalRef} closeChannelModal={closeChannelModal} />
     </div>
   );
 }
