2328a838cbb3c66de5040b2187b61d355cf844da
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-28 14:37:06 -0800

    emergency staging

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ChatPage from "./pages/ChatPage";
 import "./styles/login.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import UserProfile from "./components/UserProfile";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/register" element={<Register />} />
 
       <Route path="/chat-page" element={<ChatPage />} />
+      <Route path="/profile" element={<UserProfile />} />
     </Routes>
   );
 };
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UserProfile = () => {
+    return (
+        <div className='bg-gray-600 p-4 text-white'>
+            <div className='bg-gray-900 p-4'>
+            <p className=''>Chazz</p>
+            <hr className='my-4' />
+            <p>Member since</p>
+            <p>June 10, 2023</p>
+            <p>Note</p>
+            <hr className='my-4' />
+            </div>
+        </div>
+    )
+}
+
+export default UserProfile
