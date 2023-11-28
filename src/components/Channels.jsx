7df0b08855c8b21bb45c8f40c463433f4df79610
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-27 16:59:53 -0800

    experiment with RestructuredData for channels

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Data from "../Data/Data.json";
+import RestructuredData from '../Data/RestructuredData.json'
+import { Link } from "react-router-dom";
 
 function Channels({ handleSelect }) {
   return (
     <div className="text-white">
-      {Data.channels.map((channel) => {
+      {
+        RestructuredData.channels.map((channel) => {
+          
+        })
+      }
+      {/* {RestructuredData.channels.map((channel) => {
         return (
-          <div
-            key={channel.id}
-            className="text-white hover:bg-gray-800 cursor-pointer p-1"
-            onClick={() => handleSelect(channel)}
-          >
-            #{channel.name}
-          </div>
+          // <div
+          //   key={channel.id}
+          //   className="text-white hover:bg-gray-800 cursor-pointer p-1"
+          //   onClick={() => handleSelect(channel)}
+          // >
+          //   <Link to={`/chat-page/${channel.id}`}>#{channel.name}</Link>
+          // </div>
+          <div>
+
+            </div>
         );
-      })}
+      })} */}
     </div>
   );
 }
