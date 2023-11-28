3bad8854feee1c5ecfd013838ba8727c87609adf
Chazz Carrizales <czcarrizales@yahoo.com>
2023-11-27 16:57:34 -0800

    using state for channel id to know what to display

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../components/SideBar";
 import TextBox from "../components/TextBox";
 import ChannelMessages from "../components/channelMessages";
 //Chat page
 function ChatPage() {
+
+  const [channelID, setChannelID] = useState(1)
+
   return (
     <div className="flex flex-row h-full">
-      <SideBar />
+      <SideBar channelID={channelID} setChannelID={setChannelID} />
 
       <div className="flex flex-col h-screen w-screen bg-stone-800">
         
-        <ChannelMessages />
+        <ChannelMessages channelID={channelID} setChannelID={setChannelID} />
       </div>
     </div>
   );
