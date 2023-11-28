da7abb7d5d2efce78d8b971f22f67e3e0e6902e4
williamvelichko <williamvelichko2003@gmail.com>
2023-11-28 12:17:23 -0800

    removed free space

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
       <Route path="/chat-page" element={<ChatPage />} />
     </Routes>
   );
