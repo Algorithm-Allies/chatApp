aeb5b8c2438e069401740c920bbdd98aacb7a9ca
Chazz Carrizales <czcarrizales@yahoo.com>
2023-12-04 10:54:49 -0800

    fix code to match changed handleProfilePopup

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import ProfilePopup from './ProfilePopup';
+import React from 'react';
 
-const Message = ({ profilePic, name, lastName, message, timestamp, position, handleProfilePopup }) => {
+const Message = ({ profilePic, name, lastName, message, timestamp, handleProfilePopup }) => {
 
   return (
     <div className="flex mb-4 items-center">
@@ -9,12 +8,12 @@ const Message = ({ profilePic, name, lastName, message, timestamp, position, han
         src={profilePic}
         alt={name}
         className="w-10 h-10 rounded-full mr-4 cursor-pointer profile-popup"
-        onClick={(event) => handleProfilePopup(event, true)}
+        onClick={(event) => handleProfilePopup(event)}
       />
       <div>
-        <div className="flex items-center cursor-pointer" onClick={() => handleProfilePopup(true)}>
-          <div className="text-sm font-bold mr-2">{name}</div>
-          <div className="text-sm font-bold mr-2">{lastName}</div>
+        <div className="flex items-center cursor-pointer" onClick={(event) => handleProfilePopup(event)}>
+          <div className="text-sm font-bold mr-2 profile-popup">{name}</div>
+          <div className="text-sm font-bold mr-2 profile-popup">{lastName}</div>
           <div className="text-sm text-gray-500">{timestamp}</div>
         </div>
         <p className="text-gray-800">{message}</p>
