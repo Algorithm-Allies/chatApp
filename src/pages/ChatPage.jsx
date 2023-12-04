f7c2e13d4ea00b35407dd76d198819588ebaed39
Chazz Carrizales <czcarrizales@yahoo.com>
2023-12-04 10:54:12 -0800

    check classList for profile-popup

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -8,21 +8,17 @@ function ChatPage() {
   const [position, setPosition] = useState({x: 0, y: 0})
   const [isVisible, setIsVisible] = useState(false)
 
-  const handleProfilePopup = (event, isValid) => {
-    console.log(event)
-    console.log(isValid)
+  const handleProfilePopup = (event) => {
     setPosition({x: event.clientX, y: event.clientY})
-    if (!isValid) {
-      setIsVisible(false)
-    } else if (isValid) {
-      setIsVisible(true)
-    }
   }
 
   const checkPosition = (event) => {
-    console.log(event.target)
-    if (event.target.classList.contains('profile-popup')) {
-      console.log('this is either the profile picture or profile name')
+    if (event.target.classList.contains('profile-popup') && isVisible) {
+      setIsVisible(false)
+      console.log('popup and it is visible')
+    } else if (event.target.classList.contains('profile-popup')) {
+      setIsVisible(true)
+      console.log('popup and setting to visible')
     } else {
       setIsVisible(false)
       console.log('this is something else entirely')
