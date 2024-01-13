e3befec5cb7ad0eeb84a25a01c2015cf9c7dec08
Chazz Carrizales <czcarrizales@yahoo.com>
2024-01-12 16:57:48 -0800

    test push new channel data to Data.channels on click

--- src/components/CreateChannelModal.jsx
+++ src/components/CreateChannelModal.jsx
@@ -3,6 +3,13 @@ import Data from "../Data/Data.json";
 import "../styles/CreateChannelModal.css";
 
 function CreateChannelModal({ channelModalRef, closeChannelModal }) {
+  const handleCreateChannel = () => {
+    console.log('created channel')
+    console.log(Data.channels)
+    Data.channels.push({ "id": 777, "name": "add channel test", "serverId": 1 },)
+    console.log(Data.channels)
+    closeChannelModal()
+  }
   return (
     <dialog ref={channelModalRef}>
       <div id="channel-modal">
@@ -27,7 +34,7 @@ function CreateChannelModal({ channelModalRef, closeChannelModal }) {
           <button id="channel-modal-cancel" onClick={() => closeChannelModal()}>
             Cancel
           </button>
-          <button id="channel-modal-create">Create Channel</button>
+          <button id="channel-modal-create" onClick={handleCreateChannel}>Create Channel</button>
         </div>
       </div>
     </dialog>
