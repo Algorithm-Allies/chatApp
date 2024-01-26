0d246915d69d934cab14f8a6f031ed99b4bde380
mnicokar <nicokar.mobin@gmail.com>
2024-01-26 14:01:04 -0800

    added environment variable

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -23,7 +23,6 @@ function Login() {
     try {
       const response = await axios.post(
         `http://localhost:${import.meta.env.VITE_BACKEND_PORT}/api/users/login`,
-
         {
           email: email,
           password: password,
@@ -31,11 +30,13 @@ function Login() {
       );
 
       const { data } = response;
+      console.log(data);
 
       alert("Login success!");
 
       // Store the JWT token in local storage but you can also use cookies
       localStorage.setItem("token", data.token);
+      localStorage.setItem("userId", data._id);
 
       // Redirect the user to the chat page
       navigate("/chat-page");
