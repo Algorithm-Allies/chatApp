ee15522dc17aebc417df6ddecab4d615b8f33c09
mnicokar <67208221+mnicokar@users.noreply.github.com>
2024-01-31 14:44:17 -0800

    Revert "William/profile"

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -23,7 +23,6 @@ function Login() {
     try {
       const response = await axios.post(
         `http://localhost:${import.meta.env.VITE_BACKEND_PORT}/api/users/login`,
-
         {
           email: email,
           password: password,
