4bac921065f388828ab493b369cd42c4bd3227ce
williamvelichko <williamvelichko2003@gmail.com>
2024-02-13 09:30:59 -0800

    a little addition

--- src/Auth/ProtectedRoute.jsx
+++ src/Auth/ProtectedRoute.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-
+import { jwtDecode } from "jwt-decode";
 const ProtectedRoute = ({ Component }) => {
   const token = localStorage.getItem("token");
-  function tokenVerify() {
-    return token ? true : false;
-  }
-  const auth = tokenVerify();
+
+  const isTokenValid = () => {
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // Convert to seconds
+      return decodedToken.exp > currentTime;
+    }
+    return false; // If token doesn't exist, consider it invalid
+  };
+  const auth = token && isTokenValid();
   return auth ? <Component /> : <Navigate to="/" />;
 };
 export default ProtectedRoute;
