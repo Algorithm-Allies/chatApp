9fc7276d0065b1f41c60c9c840d98156db3ce70c
williamvelichko <williamvelichko2003@gmail.com>
2024-02-14 11:22:51 -0800

    working

--- src/components/ChannelSettingsPopup/ChannelSettingsPopup.jsx
+++ src/components/ChannelSettingsPopup/ChannelSettingsPopup.jsx
@@ -1,27 +1,42 @@
 import React from "react";
+import axios from "axios";
 
 function ChannelSettingsPopup({ onClose, selectedChannel }) {
-  const handleDeleteChannel = () => {
-    onClose();
+  const handleDeleteChannel = async () => {
+    const token = localStorage.getItem("token");
+    console.log(selectedChannel._id);
+    try {
+      const response = await axios.delete(
+        `http://localhost:3500/api/channels/${selectedChannel._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      onClose();
+    } catch (error) {
+      console.error("Error deleting channel:", error);
+    }
   };
-  console.log(selectedChannel);
 
-  const groupAdmin = selectedChannel.groupAdmin;
-  const loggenInUser = localStorage.getItem("userId");
-  console.log(groupAdmin, loggenInUser);
+  const isAdmin = selectedChannel.groupAdmin === localStorage.getItem("userId");
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-md">
         <div className="mt-4 flex justify-end">
-          <button
-            className="px-4 py-2 bg-red-500 text-white rounded-md mr-2"
-            onClick={onClose}
-          >
-            Delete
-          </button>
+          {isAdmin && (
+            <button
+              className="px-4 py-2 bg-red-500 text-white rounded-md mr-2"
+              onClick={handleDeleteChannel}
+            >
+              Delete
+            </button>
+          )}
           <button
             className="px-4 py-2 bg-gray-300 text-gray-800 rounded-md"
-            onClick={handleDeleteChannel}
+            onClick={onClose}
           >
             Cancel
           </button>
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -40,7 +40,20 @@ function Channels() {
               <span>{channel.chatName}</span>
             </div>
             <div onClick={(e) => handleOpenPopup(e, channel)}>
-              <p>del</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                />
+              </svg>
             </div>
           </div>
         </div>
