9777e7ee790a36c1f85ca7025bcfd4bcffbed63f
williamvelichko <williamvelichko2003@gmail.com>
2024-02-14 15:10:34 -0800

    comming together

--- src/components/ChannelSettingsPopup/ChannelSettingsPopup.jsx
+++ src/components/ChannelSettingsPopup/ChannelSettingsPopup.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import AddFriendToGroupChat from "./AddFriendToGroupChat/AddFriendToGroupChat";
 
 function ChannelSettingsPopup({ onClose, selectedChannel }) {
   const handleDeleteChannel = async () => {
@@ -24,22 +25,38 @@ function ChannelSettingsPopup({ onClose, selectedChannel }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-md">
-        <div className="mt-4 flex justify-end">
+      <div className="bg-white p-4 rounded-md max-w-md w-full">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-semibold text-black">Channel Settings</h2>
+          <button
+            onClick={onClose}
+            className="text-gray-600 hover:text-gray-800"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <AddFriendToGroupChat group={selectedChannel} />
+        <div className="h-10 flex flex-col items-start justify-center mt-4">
           {isAdmin && (
             <button
-              className="px-4 py-2 bg-red-500 text-white rounded-md mr-2"
               onClick={handleDeleteChannel}
+              className="p-2 bg-red-500 text-sm text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
             >
-              Delete
+              Delete Channel
             </button>
           )}
-          <button
-            className="px-4 py-2 bg-gray-300 text-gray-800 rounded-md"
-            onClick={onClose}
-          >
-            Cancel
-          </button>
         </div>
       </div>
     </div>
